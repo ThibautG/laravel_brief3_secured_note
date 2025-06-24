4b29242dd3ec6f148e1c64484347bb30cf7cfef5
ThibautG <6084433+ThibautG@users.noreply.github.com>
2025-06-24 16:09:11 +0200

    feat(note): fetch and display notes for authenticated user

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Note;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class NoteController extends Controller
 {
     public function index() {
-        $notes = Note::all();
+        $id = Auth::id();
+        $notes = Note::where('user_id', $id)->get();
         return view('dashboard', compact('notes'));
     }
 }
